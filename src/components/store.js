2cfd50f7a0637c7018ad1d85d47daf844fff80d6
Vendone <andreasventurin@gmail.com>
2022-04-17 10:55:03 +0200

    store erweitert mit routeSlice

--- src/components/store.js
+++ src/components/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../features/User/userSlice";
+import routeSlice from "../features/AddRoute/addRouteSlice";
 
 export const store = configureStore({
     reducer: {
-        user: userSlice
+        user: userSlice,
+        routes: routeSlice
     }
 });
 
