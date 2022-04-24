4568d72b89d42ac522ffce4f6b3ef1ac38ee5fd9
Vendone <andreasventurin@gmail.com>
2022-04-24 06:59:06 +0200

    React import aus slices entfernt

--- src/components/store.js
+++ src/components/store.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../features/User/userSlice";
 import routeSlice from "../features/AddRoute/addRouteSlice";
