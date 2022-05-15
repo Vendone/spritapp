408a65ec743dea576d4c3ddbac98121528ed46c5
Vendone <andreasventurin@gmail.com>
2022-05-15 12:18:35 +0200

    Tankstops von DB laden lassen eingefügt

--- src/components/TankStopCard/TankStopCard.js
+++ src/components/TankStopCard/TankStopCard.js
@@ -1,59 +1,66 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { loadTankstop, selectTankstops } from "../../features/TankStops/tankStopsSlice";
 
 export const TankStopCard = () => {
-    const store = useSelector((state) => state.tankstops);
+    const store = useSelector(selectTankstops);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(loadTankstop());
+    }, [dispatch])
 
     return (
         <div className="card">
             <h3>Tankstop Card</h3>
-            {store.map((tankstop) => (
-                <div className="entry" key={tankstop.id}>
-                    <div className="row">
-                        <div className="left"></div>
-                        <div className="content title"><span>Tankstelle:</span></div>
-                        <div className="content">{tankstop.gasstation_id}</div>
-                        <div className="right"></div>
+            {(store.isLoading) ? <div className="loader"></div> : (store.value[0] === 'Failed to fetch') ? <div className="fail">x</div> :
+                (store.value.length <= 0) ? <p>Keine Einträge vorhanden. Bitte ersten Eintrag hinzufügen</p> : store.value[0].map((tankstop) => (
+                    <div className="entry" key={tankstop.id}>
+                        <div className="row">
+                            <div className="left"></div>
+                            <div className="content title"><span>Tankstelle:</span></div>
+                            <div className="content">{tankstop.gasstation_id}</div>
+                            <div className="right"></div>
+                        </div>
+                        <div className="row">
+                            <div className="left"></div>
+                            <div className="content title"><span>Kraftstoff:</span></div>
+                            <div className="content">{tankstop.fuel}</div>
+                            <div className="right"></div>
+                        </div>
+                        <div className="row">
+                            <div className="left"></div>
+                            <div className="content title"><span>Menge:</span></div>
+                            <div className="content">{tankstop.amount}</div>
+                            <div className="right"></div>
+                        </div>
+                        <div className="row">
+                            <div className="left"></div>
+                            <div className="content title"><span>Preis pro Liter:</span></div>
+                            <div className="content">{tankstop.price}</div>
+                            <div className="right"></div>
+                        </div>
+                        <div className="row">
+                            <div className="left"></div>
+                            <div className="content title"><span>KM-Stand:</span></div>
+                            <div className="content">{tankstop.milage}</div>
+                            <div className="right"></div>
+                        </div>
+                        <div className="row">
+                            <div className="left"></div>
+                            <div className="content title"><span>Datum:</span></div>
+                            <div className="content">{tankstop.date}</div>
+                            <div className="right"></div>
+                        </div>
+                        <div className="row">
+                            <div className="left"></div>
+                            <div className="content title"><span>Auto:</span></div>
+                            <div className="content">{tankstop.car_id}</div>
+                            <div className="right"></div>
+                        </div>
                     </div>
-                    <div className="row">
-                        <div className="left"></div>
-                        <div className="content title"><span>Kraftstoff:</span></div>
-                        <div className="content">{tankstop.fuel}</div>
-                        <div className="right"></div>
-                    </div>
-                    <div className="row">
-                        <div className="left"></div>
-                        <div className="content title"><span>Menge:</span></div>
-                        <div className="content">{tankstop.amount}</div>
-                        <div className="right"></div>
-                    </div>
-                    <div className="row">
-                        <div className="left"></div>
-                        <div className="content title"><span>Preis pro Liter:</span></div>
-                        <div className="content">{tankstop.price}</div>
-                        <div className="right"></div>
-                    </div>
-                    <div className="row">
-                        <div className="left"></div>
-                        <div className="content title"><span>KM-Stand:</span></div>
-                        <div className="content">{tankstop.milage}</div>
-                        <div className="right"></div>
-                    </div>
-                    <div className="row">
-                        <div className="left"></div>
-                        <div className="content title"><span>Datum:</span></div>
-                        <div className="content">{tankstop.date}</div>
-                        <div className="right"></div>
-                    </div>
-                    <div className="row">
-                        <div className="left"></div>
-                        <div className="content title"><span>Auto:</span></div>
-                        <div className="content">{tankstop.car_id}</div>
-                        <div className="right"></div>
-                    </div>
-                </div>
-            ))}
+                ))}
             <Link to={`/tankstops`} className='dashbutton'>Bearbeiten</Link>
         </div>
     );
--- src/features/TankStops/tankStopsSlice.js
+++ src/features/TankStops/tankStopsSlice.js
@@ -1,32 +1,144 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const TANKSTOPS_URL = 'http://192.168.0.233:4001/tankstops/';
+
+export const loadTankstop = createAsyncThunk(
+    'tankstops/loadTankstops',
+    async () => {
+        try {
+            const response = await fetch(TANKSTOPS_URL);
+            const jsonResponse = await response.json();
+            return jsonResponse;
+        } catch (err) {
+            return err.message;
+        }
+    })
+
+//Daten senden
+export const postTankstop = createAsyncThunk(
+    'tankstops/postTankstops',
+    async (body) => {
+        try {
+            await axios.post(TANKSTOPS_URL, body);
+        } catch (err) {
+            return err.message;
+        }
+    }
+)
+// Daten ändern
+export const updateAsyncTankstop = createAsyncThunk(
+    'tankstops/updateAsyncTankstops',
+    async (data) => {
+        try {
+            await axios.put(TANKSTOPS_URL + data.id, data);
+        } catch (err) {
+            return err.message;
+        }
+    }
+)
+//Daten löschen
+export const deleteAsyncTankstop = createAsyncThunk(
+    'tankstops/deleteAsyncTankstops',
+    async (id) => {
+        try {
+            await axios.delete(TANKSTOPS_URL + '/' + id)
+        } catch (err) {
+            return err.message;
+        }
+    }
+)
 
 // Slice Object
 const options = {
     name: 'tankstops',
-    initialState: [
-        {
-            id: 1,
-            gasstation_id: 1,
-            fuel: 'Benzin',
-            amount: 45.65,
-            price: 1.596,
-            milage: 210035,
-            date: Date.now(),
-            user_id: 1,
-            car_id:1
-        }
-    ],
+    initialState:
+    {
+        value: [],
+        isLoading: false,
+        hasError: false
+    },
     reducers: {
-        addTankStop: (state, action) => {
-            state.push(action.payload);
+        addTankstop: (state, action) => {
+            state.value[0].unshift(action.payload);
         },
-        removeTankStop: (state, action) => {
-            state.filter(tankStop => tankStop.id !== action.payload.id)
-        }
+        updateTankstop: (state, action) => {
+            state.value[0].map((tankstop) => {
+                if (tankstop.id === action.payload.id) {
+                    tankstop.gasstation_id = action.payload.gasstation_id;
+                    tankstop.fuel = action.payload.fuel;
+                    tankstop.amount = action.payload.amount;
+                    tankstop.price = action.payload.price;
+                    tankstop.milage = action.payload.milage;
+                    tankstop.date = action.payload.date;
+                    tankstop.user_id = action.payload.user_id;
+                    tankstop.car_id = action.payload.car_id;
+                }
+                return tankstop;
+            });
+        },
+        deleteTankstop: (state, action) => {
+            state.value[0] = state.value[0].filter((tankstop) => action.payload.id !== tankstop.id);
+        },
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(loadTankstop.pending, (state, action) => {
+                state.isLoading = true;
+                state.hasError = false;
+            })
+            .addCase(loadTankstop.fulfilled, (state, action) => {
+                state.value.push(action.payload);
+                state.isLoading = false;
+                state.hasError = false;
+            })
+            .addCase(loadTankstop.rejected, (state, action) => {
+                state.isLoading = false;
+                state.hasError = true;
+            })
+            .addCase(postTankstop.pending, (state, action) => {
+                state.isLoading = true;
+                state.hasError = false;
+            })
+            .addCase(postTankstop.fulfilled, (state, action) => {
+                state.value.push(action.payload);
+                state.isLoading = false;
+                state.hasError = false;
+            })
+            .addCase(postTankstop.rejected, (state, action) => {
+                state.isLoading = false;
+                state.hasError = true;
+            })
+            .addCase(deleteAsyncTankstop.pending, (state, action) => {
+                state.isLoading = true;
+                state.hasError = false;
+            })
+            .addCase(deleteAsyncTankstop.fulfilled, (state, action) => {
+                state.value.push(action.payload);
+                state.isLoading = false;
+                state.hasError = false;
+            })
+            .addCase(deleteAsyncTankstop.rejected, (state, action) => {
+                state.isLoading = false;
+                state.hasError = true;
+            })
+            .addCase(updateAsyncTankstop.pending, (state, action) => {
+                state.isLoading = true;
+                state.hasError = false;
+            })
+            .addCase(updateAsyncTankstop.fulfilled, (state, action) => {
+                state.value.push(action.payload);
+                state.isLoading = false;
+                state.hasError = false;
+            })
+            .addCase(updateAsyncTankstop.rejected, (state, action) => {
+                state.isLoading = false;
+                state.hasError = true;
+            })
     }
 }
 
-export const tankStopsSlice = createSlice(options);
-
-export const { addTankStop, removeTankStop } = tankStopsSlice.actions;
-export default tankStopsSlice.reducer;
+export const tankstopsSlice = createSlice(options);
+export const selectTankstops = (state) => state.tankstops;
+export const { addTankstop, updateTankstop, deleteTankstop } = tankstopsSlice.actions;
+export default tankstopsSlice.reducer;
