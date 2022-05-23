eadd5e79f7ba9243574e16c9de5c6bb4a1bc8a86
Vendone <andreasventurin@gmail.com>
2022-05-23 18:20:30 +0200

    fehler beim carstore behoben

--- src/components/CarCard/CarCard.js
+++ src/components/CarCard/CarCard.js
@@ -15,7 +15,7 @@ export const CarCard = () => {
         <div className="card">
             <h3>Car Card</h3>
             {(store.isLoading) ? <div className="loader"></div> : (store.value[0] === 'Failed to fetch' || store.value[0] === 'Unexpected token < in JSON at position 0') ? <div className="fail">x</div> :
-                (store.value[0].error === "something broke") ? <p>Keine Einträge vorhanden. Bitte ersten Eintrag hinzufügen</p> : store.value[0].map((car) => (
+                (store.value.length <= 0) ? <p>Keine Einträge vorhanden. Bitte ersten Eintrag hinzufügen</p> : store.value[0].map((car) => (
                     <div className="entry" key={car.id}>
                         <div className="row">
                             <div className="left"></div>
