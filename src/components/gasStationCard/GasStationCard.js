1659fa7ec58223841b2e714215c71d955aca6f1a
Vendone <andreasventurin@gmail.com>
2022-06-16 07:59:20 +0200

    store abfrage angepasst

--- src/components/gasStationCard/GasStationCard.js
+++ src/components/gasStationCard/GasStationCard.js
@@ -15,7 +15,7 @@ export const GasstationCard = () => {
         <div className="card">
             <h3>Tankstellen Card</h3>
             {(store.isLoading) ? <div className="loader"></div> : (store.value[0] === 'Failed to fetch' || store.value[0] === 'Unexpected token < in JSON at position 0') ? <div className="fail">x</div> :
-                (store.value.length <= 0) ? <p>Keine Einträge vorhanden. Bitte ersten Eintrag hinzufügen</p> : store.value[0].results[0].map((gasstation) => (
+                (store.value.length <= 0) ? <p>Keine Einträge vorhanden. Bitte ersten Eintrag hinzufügen</p> : store.value[0].results.map((gasstation) => (
                     <div className="entry" key={gasstation.id}>
                         <div className="row">
                             <div className="left"></div>
